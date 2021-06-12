dd175c8c30c1837cc3aa8d454d53e3fad175bce3
Markus Martens <markus.martens@gmx.net>
2021-06-12 16:24:24 +0200

    update

--- test3.sh
+++ test3.sh
@@ -1,15 +1,23 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 import usb.core
 import usb.util
+import signal
+import numpy as np
 from time import sleep
 
 TOYPAD_INIT = [0x55, 0x0f, 0xb0, 0x01, 0x28, 0x63, 0x29, 0x20, 0x4c, 0x45, 0x47, 0x4f, 0x20, 0x32, 0x30, 0x31, 0x34, 0xf7, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
 
-OFF   = [0,0,0]
-RED   = [255,0,0]
-GREEN = [0,255,0]
-BLUE  = [0,0,255]
+OFF    = [0x00,0x00,0x00]
+RED    = [0xFF,0x00,0x00]
+GREEN  = [0x00,0xFF,0x00]
+BLUE   = [0x00,0x00,0xFF]
+ORANGE = [0xFF,0x11,0x00]
+YELLOW = [0xAA,0x33,0x00]
+PINK   = [0xFF,0x00,0x33]
+TEAL   = [0x33,0xFF,0xFF]
+PURPLE = [0x33,0x00,0xFF]
+WHITE  = [0xFF,0xFF,0xFF]
 
 ALL_PADS   = 0
 CENTER_PAD = 1
@@ -21,10 +29,14 @@ TAG_INSERTED = 0
 TAG_REMOVED  = 1
 
 # UIDs can be retrieved with Android App (most probably in hexadecimal)
-uidGandalf = [4, 22, 125, 210, 161, 64, 129] # LEGO Dimension Gandalf
-uidWildStyle = [4, 77, 178, 250, 157, 72, 128]
-uidBadman = [4, 243, 67, 154, 19, 67, 128]
-uidCustom01 = [4, 71, 49, 242, 124, 73, 129]
+uidGandalf      = [4, 22, 125, 210, 161, 64, 129] # LEGO Dimension Gandalf
+uidWildStyle    = [4, 77, 178, 250, 157, 72, 128]
+uidBadman       = [4, 243, 67, 154, 19, 67, 128]
+uidAbbey        = [4, 103, 114, 146, 40, 73, 128]
+uidCustom01     = [4, 71, 49, 242, 124, 73, 129]
+uidBadCop       = [0x40,0x41,0x65,0x1a,0xa3,0x48,0x80]
+uidBenny        = [0x40,0x9E,0xE0,0x32,0x5C,0x49,0x84]
+uidChase        = [0x40,0x73,0xFF,0x5A,0xA7,0x4A,0x80]
 
 def init_usb():
     global dev
@@ -32,12 +44,12 @@ def init_usb():
     dev = usb.core.find(idVendor=0x0e6f, idProduct=0x0241)
 
     if dev is None:
-        print 'Device not found'
+        print('Device not found')
     else:
         if dev.is_kernel_driver_active(0):
             dev.detach_kernel_driver(0)
 
-        print usb.util.get_string(dev, dev.iProduct)
+        print((usb.util.get_string(dev, dev.iProduct)))
 
         dev.set_configuration()
         dev.write(1,TOYPAD_INIT)
@@ -78,21 +90,31 @@ def uid_compare(uid1, uid2):
     return match 
 
 def tag_inserted(pad_num,uid_bytes):
-    print 'inserted',pad_num,uid_bytes
-    match = uid_compare(uid_bytes, uidCustom01)
-    if match:
-        switch_pad(pad_num, GREEN)
-    else:
-        switch_pad(pad_num, RED)
+    print(np.array(uid_bytes),'detected on pad',pad_num)
+    if uid_compare(uid_bytes, uidCustom01): switch_pad(pad_num, WHITE)
+    elif uid_compare(uid_bytes, uidGandalf): switch_pad(pad_num, YELLOW)
+    elif uid_compare(uid_bytes, uidBadCop): switch_pad(pad_num, ORANGE)
+    elif uid_compare(uid_bytes, uidWildStyle): switch_pad(pad_num, PINK)
+    elif uid_compare(uid_bytes, uidBadman): switch_pad(pad_num, BLUE)
+    elif uid_compare(uid_bytes, uidAbbey): switch_pad(pad_num, TEAL)
+    elif uid_compare(uid_bytes, uidBenny): switch_pad(pad_num, GREEN)
+    elif uid_compare(uid_bytes, uidChase): switch_pad(pad_num, PURPLE)
+    else: switch_pad(pad_num, RED)
     return
 
 def tag_removed(pad_num,uid_bytes):
-    print 'removed',pad_num,uid_bytes
+    print(np.array(uid_bytes),'removed from pad',pad_num)
     switch_pad(pad_num, OFF)
     return
 
+def signal_term_handler(signal, frame):
+    print('got SIGTERM')
+    exit(0)
+
 def main():
     init_usb()
+    np.set_printoptions(formatter={'int':hex})
+    signal.signal(signal.SIGTERM, signal_term_handler)
     if dev != None :
         while True:
             try:
@@ -112,10 +134,12 @@ def main():
                     else:
                         tag_removed(pad_num,uid_bytes)
 
-            except usb.USBError, err:
+            except usb.USBError as err:
                 pass
-
-        switch_pad(ALL_PADS,OFF)
+            except KeyboardInterrupt:
+                print('interrupted')
+            finally:
+                switch_pad(ALL_PADS,OFF)
     return
 
 if __name__ == '__main__':
